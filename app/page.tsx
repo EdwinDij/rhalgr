9e465542d7caaa0ad866c600f488110653d09adf
EdwinDij <edwin.d899@gmail.com>
2026-05-24 18:27:40 +0200

    feat: changement du layout pour la navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
-import Navbar from "./components/navbar/Navbar";
-
 export default function Home() {
   return (
     <>
-      <header>
-        <nav>
-          <Navbar />
-        </nav>
-      </header>
       <main>{/* main content */}</main>
       <footer>{/* footer content */}</footer>
     </>
